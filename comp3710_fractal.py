654472f2b042211c89d9417f5c0d396ef63a9c75
grumpyoldking <ant0n.huang@outlook.com>
2025-08-17 16:27:52 +1000

    commented

--- comp3710_fractal.py
+++ comp3710_fractal.py
@@ -3,21 +3,24 @@ import matplotlib.pyplot as plt
 
 
 def grid(nrows):
-
+    # Generate a grid of points in a triangular lattice
     n = torch.arange(nrows, dtype=torch.int32)
     k = torch.arange(nrows, dtype=torch.int32)
-
+    # Create a meshgrid of n and k 
     N = n.view(-1,1)
     K = k.view(1,-1)
 
     #check if grid is in triangle 
     valid = K <= N 
 
+    #check if k is odd
     N_minus_K = torch.where(valid, N-K, torch.zeros_like(K))
     odd = ((k & N_minus_K) == 0) & valid 
 
+    #convert to float32 for plotting
     return odd.to(torch.float32)
 
+# Plot the grid using matplotlib
 def plot_grid(img):
     plt.imshow(img, cmap='binary', interpolation='nearest')
     plt.axis('off')
