a10a0a0242d8dab15abb15fd2b731b6916726d19
grumpyoldking <ant0n.huang@outlook.com>
2025-08-22 08:05:35 +1000

    updated comments

--- comp3710_fractal.py
+++ comp3710_fractal.py
@@ -17,8 +17,7 @@ def grid(nrows):
     N_minus_K = torch.where(valid, N-K, torch.zeros_like(K))
     odd = ((k & N_minus_K) == 0) & valid 
 
-    #convert to float32 for plotting
-    return odd.to(torch.float32)
+    return odd
 
 # Plot the grid using matplotlib
 def plot_grid(img):
@@ -26,11 +25,10 @@ def plot_grid(img):
     plt.axis('off')
     plt.show()
 
-if __name__ == "__main__":
-    nrows = 2048 
-    img = grid(nrows)
-    plot_grid(img)
-    print("Grid generated and displayed.")
+nrows = 1024
+img = grid(nrows)
+plot_grid(img)
+
 
 
 
